de3354a3405fc932b5e5fa9ead31e2f47348ad02
pxphbn8 <160334114+pxphbn8@users.noreply.github.com>
2024-10-22 22:57:57 +0700

    Update Student.js

--- student-management/models/Student.js
+++ student-management/models/Student.js
@@ -1,1 +1,10 @@
+const mongoose = require("mongoose");
+
+const studentSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    studentId: { type: String, required: true },
+    dateOfBirth: { type: Date, required: true }
+});
+
+module.exports = mongoose.model("Student", studentSchema);
 
